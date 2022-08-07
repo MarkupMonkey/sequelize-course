a5de76c1c13b25aa4f0164a1e5ad4099765d4e79
Marco <99331256+MarkupMonkey@users.noreply.github.com>
2022-08-07 14:38:48 +0200

    Create utils for JWT

--- src/config/environment.js
+++ src/config/environment.js
@@ -5,7 +5,7 @@ export default {
     jwtAccessTokenSecret:
         process.env.JWT_ACCESS_TOKEN_SECRET ||
         '08054dd842392d36d3364ce1a9851560792ebfe00f58573868602b5b85f297e4',
-    jwtRefreshtokenSecret:
+    jwtRefreshTokenSecret:
         process.env.JWT_REFRESH_TOKEN_SECRET ||
         '37786bfe4c851594d6bf194b50c2462019aca077a7567b0a1be5fd958903a951',
 
