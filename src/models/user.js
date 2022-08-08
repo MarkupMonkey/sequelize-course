a32632d95d2b503107174e9477e5e92801c405a7
Marco <99331256+MarkupMonkey@users.noreply.github.com>
2022-08-08 14:59:56 +0200

    Create user model (part 2)

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,6 @@
 import { Model, DataTypes } from 'sequelize';
+import bcrypt from 'bcrypt';
+import environment from '../config/environment';
 
 export default (sequelize) => {
     class User extends Model {
@@ -7,7 +9,9 @@ export default (sequelize) => {
             User.Roles = User.hasMany(models.Role)
         }
 
-        static async hashPassword(password) { };
+        static async hashPassword(password) {
+            return bcrypt.hash(password, environment.saltRounds);
+        };
 
         static async createNewUser({  // metodi statici che implementeremo
             email,
@@ -20,48 +24,72 @@ export default (sequelize) => {
         }) { }
     }
 
-    User.init({
-        email: {
-            type: DataTypes.STRING(100),
-            allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: {
-                    msn: 'Not a valid email address, sorry.'
+    User.init(
+        {
+            email: {
+                type: DataTypes.STRING(100),
+                allowNull: false,
+                unique: true,
+                validate: {
+                    isEmail: {
+                        msn: 'Not a valid email address, sorry.'
+                    },
+                },
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            username: {
+                type: DataTypes.STRING(50),
+                unique: true,
+                validate: {
+                    len: {
+                        args: [2, 50],
+                        msg: 'Username must contain between 2 an 50 characters',
+                    },
+                },
+            },
+            firstName: {
+                type: DataTypes.STRING(50),
+                validate: {
+                    len: {
+                        args: [3, 50],
+                        msg: 'First name must contain between 3 an 50 characters',
+                    },
+                },
+            },
+            lastName: {
+                type: DataTypes.STRING(50),
+                validate: {
+                    len: {
+                        args: [3, 50],
+                        msg: 'Last name must contain between 3 an 50 characters',
+                    },
                 },
             },
         },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        username: {
-            type: DataTypes.STRING(50),
-            unique: true,
-            validate: {
-                len: {
-                    args: [2, 50],
-                    msg: 'Username must contain between 2 an 50 characters',
+        {
+            sequelize, modelName: 'User', defaultScope: { attributes: { exclude: ['password'] } },
+            scopes: {
+                withPassword: {
+                    attributes: { include: ['password'] },
                 },
             },
-        },
-        firstName: {
-            type: DataTypes.STRING(50),
-            validate: {
-                len: {
-                    args: [3, 50],
-                    msg: 'First name must contain between 3 an 50 characters',
-                },
-            },
-        },
-        lastName: {
-            type: DataTypes.STRING(50),
-            validate: {
-                len: {
-                    args: [3, 50],
-                    msg: 'Last name must contain between 3 an 50 characters',
-                },
-            },
-        },
-    }, { sequelize, modelName: 'User' });
+        }
+    );
+
+    // user = await User.findOne({where: {email: 'test@example.com}})
+    // user.email, user.username, user.firstName, user.lastName, user.password
+    // await user.comparePassword('test123#')
+    User.prototype.comparePasswords = async function (password) {
+        return bcrypt.compare(password, this.password);
+    }
+
+    User.beforeSave(async (user, option) => {
+        const hashedPassword = await User.hashPassword(user.password);
+        user.password = hashedPassword;
+    })
+
+    return User;
 };
