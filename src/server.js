5c32de6d5a3fff92c0b24ea02f2b68c486fdf1f7
Marco <99331256+MarkupMonkey@users.noreply.github.com>
2022-08-11 16:55:38 +0200

    Listening for connections

--- src/server.js
+++ src/server.js
@@ -2,12 +2,17 @@ import './config';
 import Database from './database';
 import environment from './config/environment';
 import dbConfig from './config/database';
+import App from './app';
 
 // IIFE = Immediately Invoked Function Expression
 (async () => {
     try {
         const db = new Database(environment.nodeEnv, dbConfig);
         await db.connect();
+
+        const App = require('./app').default;
+        const app = new App();
+        app.listen();
     } catch (err) {
         console.error('Something went wrong when initializing the server:\n', err.stack);
     }
